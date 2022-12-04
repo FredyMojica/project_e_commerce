ed281e33ad2929bea33aa8dfd9968b224dd3c518
FredyMojica <fremo22@gmail.com>
2022-12-04 10:06:06 -0500

    back endpoint ok y front conectado ok

--- project_c4/src/App.js
+++ project_c4/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Admin from './componentes/Admin';
 import CrearCuenta from './componentes/CrearCuenta';
+import Home from './componentes/Home';
 import Login from './componentes/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" exact element ={<Login/>}  />
+          <Route path="/" exact element ={<Home/>}  />
+          <Route path="/login" exact element ={<Login/>}  />
           <Route path="/crear-cuenta" exact element ={<CrearCuenta/>}  />
+          <Route path="/admin" exact element ={<Admin/>}  />
       </Routes>
     </Router>
   );
