65f91f724bbf7c0ce48535e8f7fb7f4f2704c6eb
FredyMojica <fremo22@gmail.com>
2022-12-16 15:15:11 -0500

    Revert "Agregué crud productoControllers"
    This reverts commit cd12ae8e0f63f105abb235b2a1bc3b40f51b57d0.

--- Back/controllers/productoController.js
+++ Back/controllers/productoController.js
@@ -1,73 +1,14 @@
-const Categoria = require("../models/categoria");
-const Producto = require("../models/producto");
+const producto = require("../models/producto");
 
-//req: lo q se puede leer desde postman
-//res: lo qe se envía hacia postman
-
-exports.leerProducto = async(req, res) =>{
-    try{
-        const producto = await Producto.find({creador : req.usuario.id});
-        res.json({producto});
-    }catch(error){
-        console.log(error);
-    }
+exports.leerProducto = async ( req, res ) => {
+    res.json({ msg: "ejecutó leer Producto"});
 }
-exports.crearProducto = async(req, res) =>{
-    
-    const {categoriaId} = req.body;
-    try{
-        const categoriaExiste = await Categoria.findById(categoriaId);
-        if(!categoriaExiste){
-            return res.status(400).json({msg:"Ingrese una categoría válida."});
-        }
-
-        const producto = new Producto(req.body);
-        await producto.save();
-
-        res.json(producto);
-        
-    }catch(error){
-        console.log(error);
-    }
+exports.crearProducto = async ( req, res ) => {
+    res.json({ msg: "ejecutó crear Producto"});    
 }
-exports.actualizarProducto = async(req, res) =>{
-    const {id} = req.params;
-    const producto = await Producto.findById(id);
-    if(!producto){
-        return res.status(404).json({msg:"Producto no encontrado"});
-    }
-
-    // if(producto.creador.toString() !== req.usuario.id.toString()){
-    //     return res.status(400).json({msg:"Acción no válida para esta categoría"});
-    // }
-
-    producto.nombre = req.body.nombre || producto.nombre;
-    producto.descripcion = req.body.descripcion || producto.descripcion;
-    producto.stock = req.body.stock || producto.stock;
-    producto.precio = req.body.stock || producto.precio;
-    producto.imagen = req.body.imagen || producto.imagen;
-    producto.save();
-    res.json({producto});
+exports.actualizarProducto = async ( req, res ) => {
+    res.json({ msg: "ejecutó actualizar Producto"});    
 }
-exports.borrarProducto = async(req, res) =>{
-    try{
-        await Producto.deleteOne({_id: req.params.id}); //
-        res.json({msg: "Producto eliminado"});
-    }catch(error){
-        console.log(error);
-    }
+exports.borrarProducto = async ( req, res ) => {
+    res.json({ msg: "ejecutó borrar Producto"});    
 }
-
-
-// exports.leerProducto = async(req, res) =>{
-//     res.json({msg:"aquí se ejecutó leer producto"});
-// }
-// exports.crearProducto = async(req, res) =>{
-//     res.json({msg:"aquí se ejecutó crearc producto"});
-// }
-// exports.actualizarProducto = async(req, res) =>{
-//     res.json({msg:"aquí se ejecutó actualizar producto"});
-// }
-// exports.borrarProducto = async(req, res) =>{
-//     res.json({msg:"aquí se ejecutó borrar producto"});
-// }
